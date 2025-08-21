442a6cbbd80d3e4a6df6fd890ff3c8e3b49fb1c9
amelo171017-lgtm <amelo171017@gmail.com>
2025-08-21 17:32:21 -0300

    New Push

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'co2trola-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './icon.png',
+  './background.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
